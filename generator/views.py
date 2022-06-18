942d82d3fe27324e104100d1416e520cb5c5d862
Pirogok <dimbosa@yandex.by>
2022-06-18 19:18:43 +0300

    Final design and backend

--- generator/views.py
+++ generator/views.py
@@ -5,15 +5,28 @@ import random
 def home(request):
     return render(request,'generator/home.html')
 
+def about(request):
+    return render(request,'generator/about.html')
+
 def password(request):
 
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length',8))
 
     thepassword = ''
     for i in range(length):
         thepassword += random.choice(characters)
     return render(request,'generator/password.html', {'password':thepassword})
+
 
